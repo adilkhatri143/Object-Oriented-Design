780f0ed532458f70fb9e657de562db3a611dff72
Adil Khatri <adil009ik@gmail.com>
2021-02-14 19:14:18 +0530

    first commit

---
+++ LRU Cache/cache.py
@@ -0,0 +1,52 @@
+from node import Node
+from linked_list import LinkedList
+
+
+class Cache:
+    def __init__(self, MAX_SIZE):
+        self.MAX_SIZE = MAX_SIZE
+        self.size = 0
+        self.lookup = {}
+        self.linked_list = LinkedList()
+
+    def get_data(self, query):
+        exist = self.lookup.get(query)
+
+        if exist is None:
+            return None
+
+        node = self.linked_list.find_node(query)
+        self.linked_list.move_to_front(node)
+        return node.data
+
+    def set_data(self, query, result):
+        exist = self.lookup.get(query)
+
+        if exist is not None:
+            node = self.linked_list.find_node(query)
+            self.lookup[query] = result
+            node.data = result
+            self.linked_list.move_to_front(node)
+        else:
+            if self.size == self.MAX_SIZE:
+                self.lookup.pop(self.linked_list.tail.query, None)
+                self.linked_list.remove_from_tail()
+            else:
+                self.size += 1
+
+            new_node = Node(query, result)
+            self.linked_list.append_to_front(new_node)
+            self.lookup[query] = result
+
+
+if __name__ == '__main__':
+    cache = Cache(3)
+    cache.set_data("first", 1)
+    cache.set_data("second", 2)
+    cache.set_data("third", 3)
+    cache.set_data("fourth", 4)
+    print(cache.get_data("first"))
+    print(cache.get_data("second"))
+    print(cache.lookup)
+    print(cache.linked_list.head.data)
+    print(cache.linked_list.tail.data)
---
+++ LRU Cache/linked_list.py
@@ -0,0 +1,57 @@
+from node import Node
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def move_to_front(self, node):
+        temp = self.head
+        before_temp = Node(0, 0)
+        while temp.link != None:
+            before_temp = temp
+            temp = temp.link
+
+        if temp == self.head:
+            return
+        elif temp.link == None:
+            before_temp.link = None
+            temp.link = self.head
+            self.head = temp
+            self.tail = before_temp
+        else:
+            before_temp.link = temp.link
+            temp.link = self.head
+            self.head = temp
+
+    def append_to_front(self, node):
+        if self.tail == None and self.head == None:
+            self.head = node
+            self.tail = node
+        else:
+            node.link = self.head
+            self.head = node
+
+    def remove_from_tail(self):
+        temp = self.head
+        count = 1
+        while temp.link != None and temp.link.link != None:
+            temp = temp.link
+            count += 1
+
+        if count == 1:
+            self.head = None
+            self.tail = None
+            return
+
+        self.tail = temp
+        del temp.link
+        self.tail.link = None
+
+    def find_node(self, query):
+        temp = self.head
+        while temp != None and temp.query != query:
+            temp = temp.link
+
+        return temp
